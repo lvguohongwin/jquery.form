508d240d0165aa296d91a1dabb9cb16595a7c97a
Maciej Malecki <smt116@gmail.com>
2012-09-19 21:00:35 +0200

    utworzenie klasy odpowiedzialnej za przekazywanie informacji z php do js

--- php/post.php
+++ php/post.php
@@ -7,3 +7,25 @@
  * 
  * MIT License http://www.opensource.org/licenses/mit-license.php
  */
+
+error_reporting(NULL);
+
+/**
+ * Klasa odpowiedzialna za przekazanie informacji z php do js 
+ */
+final class json {
+
+    /**
+     * @param array $array tablica do przekazania do js
+     */
+    function __construct($array) {
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Pragma: no-cache");
+        header("Content-type: application/json");
+        echo json_encode($array);
+    }
+
+}
+
